61d8cd3a0192973e6f8d86bd53bf17fed788cf5b
touchpoint1 <155140803+touchpoint1@users.noreply.github.com>
2023-12-29 08:38:48 +0100

    Add files via upload

---
+++ responsive_services/tpointPortfolio/responsive_services/README.md
@@ -0,0 +1,2 @@
+# responsive_services
+ htmlcss
---
+++ responsive_services/tpointPortfolio/services.php
@@ -0,0 +1,63 @@
+<?php
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+    <title>OUR SERVICES</title>
+    <link rel="stylesheet" href="services.css">
+</head>
+<body>
+<div class="container">
+    <h1 class="heading">OUR SERVICES</h1>
+    <div class="box-container">
+        <div class="box">
+            <img src="images/html5.png" alt="">
+            <h3>HTML 5</h3>
+            <p>TOUCHPOINT is a group of experts in HTML5, the latest standard for web development. HTML5 is a powerful and versatile technology that allows you to create rich and interactive websites, apps, games, and more. Whether you need a simple landing page, a complex e-commerce platform, or a cutting-edge multimedia experience, TOUCHPOINT can help you achieve your goals.</p>
+            <a href="" class="btn">READ MORE</a>
+        </div>
+        <div class="box">
+            <img src="images/Javascript_badge.svg.png" alt="">
+            <h3>JAVASCRIPT</h3>
+            <p>JavaScript is a dynamic and versatile technology that allows you to create interactive and responsive websites, apps, games, and more. Whether you need a simple animation, a complex web application, or a cutting-edge user interface, TOUCHPOINT can help you achieve your goals.</p>
+            <a href="" class="btn">READ MORE</a>
+        </div>
+        <div class="box">
+            <img src="images/graphics.png" alt="">
+            <h3>GRAPHICS DESIGN</h3>
+            <p>the art and practice of creating visual communication. Graphic design is a powerful and essential tool that allows you to convey your message, identity, and values to your audience. Whether you need a logo, a brochure, a poster, a website, or any other graphic product, TOUCHPOINT can help you achieve your goals.</p>
+            <a href="" class="btn">READ MORE</a>
+        </div>
+        <div class="box">
+            <img src="images/phplogo.png" alt="">
+            <h3>PHP</h3>
+            <p>PHP is a flexible and powerful technology that allows you to create dynamic and interactive websites, apps, games, and more. Whether you need a simple blog, a complex content management system, or a cutting-edge e-commerce platform, TOUCHPOINT can help you achieve your goals.</p>
+            <a href="" class="btn">READ MORE</a>
+        </div>
+        <div class="box">
+            <img src="images/React-Symbol.png" alt="">
+            <h3>REACT</h3>
+            <p>Are you looking for a professional and reliable web developer who can create stunning and responsive websites using React? If yes, then you have come to the right place!</p>
+            <a href="" class="btn">READ MORE</a>
+        </div>
+        <div class="box">
+            <img src="images/android.png" alt="">
+            <h3>ANDROID</h3>
+            <p>Do you need a skilled and experienced android developer who can create amazing and user-friendly apps for your business or personal use? If yes, then look no further! As TOUCHPOINT has got a group of young developers who can help you get the most eliable and effective android application for your business</p>
+            <a href="" class="btn">READ MORE</a>
+        </div>
+    </div>
+
+
+
+
+
+</div>
+</body>
+</html>
